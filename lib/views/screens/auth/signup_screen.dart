97be6f02a21df44da45485371732069f95fc9ba7
XArchangel1 <nakshatrasemwal1@gmail.com>
2022-07-19 20:25:28 +0530

    register user complete

--- lib/views/screens/auth/signup_screen.dart
+++ lib/views/screens/auth/signup_screen.dart
@@ -12,131 +12,137 @@ class SignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Tik-Tok Clone",
-              style: TextStyle(
-                fontSize: 35,
-                color: buttonColor,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-            Text(
-              'Register',
-              style: TextStyle(
-                fontSize: 35,
-                color: buttonColor,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-            Stack(
-              children: [
-                const CircleAvatar(
-                  radius: 64,
-                  backgroundImage: NetworkImage(
-                      "https://imgs.search.brave.com/m_srtgVIT7gA6mf81x9Sl8nvn18pdevEk3Ggw9I2alI/rs:fit:717:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5s/c2xGOG9BMWNMWXFY/UGNQYnE3T2d3SGFF/NSZwaWQ9QXBp"),
-                  backgroundColor: Colors.black,
-                ),
-                Positioned(
-                  bottom: -10,
-                  left: 80,
-                  child: IconButton(
-                    onPressed: ()=>auth.pickImage(),
-                    icon: const Icon(Icons.add_a_photo),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextInputField(
-                controller: _userNamecontroller,
-                labelText: 'UserName',
-                icon: Icons.person,
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextInputField(
-                controller: _emailcontroller,
-                labelText: 'Email',
-                icon: Icons.email,
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextInputField(
-                controller: _passwordcontroller,
-                labelText: 'Password',
-                icon: Icons.lock,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 50,
-              decoration: BoxDecoration(
-                color: buttonColor,
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(5),
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Tik-Tok Clone",
+                style: TextStyle(
+                  fontSize: 35,
+                  color: buttonColor,
+                  fontWeight: FontWeight.w900,
                 ),
               ),
-              child: InkWell(
-                onTap: () => auth.registerUser(_userNamecontroller.text,
-                    _emailcontroller.text, _passwordcontroller.text, auth.profilePhoto),
-                child: const Center(
-                  child: Text(
-                    'Register',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
+              Text(
+                'Register',
+                style: TextStyle(
+                  fontSize: 35,
+                  color: buttonColor,
+                  fontWeight: FontWeight.w900,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            InkWell(
-              onTap: () {
-                print("Navigating User");
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              Stack(
                 children: [
-                  const Text(
-                    "Already have an account?  ",
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
+                  const CircleAvatar(
+                    radius: 64,
+                    backgroundImage: NetworkImage(
+                        "https://imgs.search.brave.com/m_srtgVIT7gA6mf81x9Sl8nvn18pdevEk3Ggw9I2alI/rs:fit:717:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5s/c2xGOG9BMWNMWXFY/UGNQYnE3T2d3SGFF/NSZwaWQ9QXBp"),
+                    backgroundColor: Colors.black,
                   ),
-                  Text(
-                    'Login',
-                    style: TextStyle(fontSize: 20, color: buttonColor),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: () => auth.pickImage(),
+                      icon: const Icon(Icons.add_a_photo),
+                    ),
                   ),
                 ],
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextInputField(
+                  controller: _userNamecontroller,
+                  labelText: 'UserName',
+                  icon: Icons.person,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextInputField(
+                  controller: _emailcontroller,
+                  labelText: 'Email',
+                  icon: Icons.email,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextInputField(
+                  controller: _passwordcontroller,
+                  labelText: 'Password',
+                  isObscure: true,
+                  icon: Icons.lock,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: buttonColor,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+                child: InkWell(
+                  onTap: () => auth.registerUser(
+                      _userNamecontroller.text,
+                      _emailcontroller.text,
+                      _passwordcontroller.text,
+                      auth.profilePhoto),
+                  child: const Center(
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              InkWell(
+                onTap: () {
+                  print("Navigating User");
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have an account?  ",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      'Login',
+                      style: TextStyle(fontSize: 20, color: buttonColor),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/views/widgets/text_input_field.dart
+++ lib/views/widgets/text_input_field.dart
@@ -1,5 +1,3 @@
-import 'dart:isolate';
-
 import 'package:flutter/material.dart';
 import 'package:tik_tok/constants.dart';
 
